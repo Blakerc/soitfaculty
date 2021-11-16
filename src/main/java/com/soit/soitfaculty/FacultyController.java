ba15a181b2ddaad2b6cdd1a43051216a1661937f
Ruairi Blake <76410753+Blakerc@users.noreply.github.com>
2021-11-16 15:13:19 -0500

    class work

--- src/main/java/com/soit/soitfaculty/FacultyController.java
+++ src/main/java/com/soit/soitfaculty/FacultyController.java
@@ -1,51 +1,104 @@
 package com.soit.soitfaculty;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import javax.annotation.PostConstruct;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import com.soit.soitfaculty.model.Faculty;
+import com.soit.soitfaculty.entity.Faculty;
+import com.soit.soitfaculty.service.FacultyService;
 
 @Controller
 @RequestMapping("/Faculties")
 public class FacultyController {
-
-	//upload faculty info
-	private List<Faculty> theFaculties;
+//
+//	//upload faculty info
+//	private List<Faculty> theFaculties;
+//	
+//	@PostConstruct
+//	private void loadData() {
+//		
+//		
+//		//create faculties
+//		Faculty fac1 = new Faculty(1, "Kelly", "Miller", "kelly@uc.edu");
+//		Faculty fac2 = new Faculty(2, "Robert", "Lee", "Robert@uc.edu");
+//		Faculty fac3 = new Faculty(3, "laura", "West", "laura@uc.edu");
+//		
+//		//create list
+//		theFaculties = new ArrayList<>();
+//		
+//		//Add or list 
+//		theFaculties.add(fac1);
+//		theFaculties.add(fac2);
+//		theFaculties.add(fac3);
+//		
+//	}
 	
-	@PostConstruct
-	private void loadData() {
-		
-		
-		//create faculties
-		Faculty fac1 = new Faculty(1, "Kelly", "Miller", "kelly@uc.edu");
-		Faculty fac2 = new Faculty(2, "Robert", "Lee", "Robert@uc.edu");
-		Faculty fac3 = new Faculty(3, "laura", "West", "laura@uc.edu");
-		
-		//create list
-		theFaculties = new ArrayList<>();
-		
-		//Add or list 
-		theFaculties.add(fac1);
-		theFaculties.add(fac2);
-		theFaculties.add(fac3);
-		
+	public FacultyService facultyService;
+	
+	public FacultyController(FacultyService theFacultyService) {
+		facultyService = theFacultyService;
 	}
 	
 	//mapping for /list
 	@GetMapping("/List")
 	public String listFaculties(Model theModel) {
 		
+		//Retrieve faculties from database
+		List<Faculty> theFaculties = facultyService.findAll();
+		
+		
 		//add faculties to the spring model
 		theModel.addAttribute("faculties", theFaculties);
 		
-		return "list-faculties";
+		return "faculties/list-faculties";
 		
 	}
+	
+	@GetMapping("/viewAddForm")
+	public String viewAddForm(Model theModel) {
+		
+		//model attribute for data binding
+		Faculty theFaculty = new Faculty();
+		
+		theModel.addAttribute("faculties", theFaculty);
+		
+		return "faculties/faculty-form";
+	}
+	
+	@GetMapping("/viewUpdateForm")
+	public String viewUpdateForm(@RequestParam("facultyId") int theId, Model theModel) {
+		
+		//Retrieve the faculty
+		Faculty theFaculty = facultyService.findById(theId);
+		
+		//prepopulate the form by setting the caulty as a model attribute
+		
+		theModel.addAttribute("faculty", theFaculty);
+		
+		//redirect to form
+		
+		return "faculties/faculty-form";
+		
+	}
+	
+	@PostMapping("/save")
+	public String saveFaculty(@ModelAttribute("faculty") Faculty theFaculty) {
+		
+		//register the faculty
+		facultyService.save(theFaculty);
+		
+		//block duplicate submission for accidental page refresh
+		return "redirect:/Faculties/List";
+		
+	}
+	
+	
+	
+	
 }
---
+++ src/main/java/com/soit/soitfaculty/service/FacultyServiceImpl.java
@@ -0,0 +1,61 @@
+package com.soit.soitfaculty.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.soit.soitfaculty.dao.FacultyRepository;
+import com.soit.soitfaculty.entity.Faculty;
+
+@Service
+public class FacultyServiceImpl implements FacultyService {
+	
+	
+	private FacultyRepository facultyRepository;
+	
+	@Autowired
+	public FacultyServiceImpl (FacultyRepository theFacultyRepository) {
+		facultyRepository = theFacultyRepository;
+	}
+
+	@Override
+	public List<Faculty> findAll() {
+		// TODO Auto-generated method stub
+		return facultyRepository.findAllByOrderByLastNameAsc();
+	}
+
+	@Override
+	public Faculty findById(int theId) {
+		// TODO Auto-generated method stub
+		
+		Optional<Faculty> facultyid = facultyRepository.findById(theId);
+		
+		Faculty theFaculty = null;
+		
+		if(facultyid.isPresent()) {
+			theFaculty = facultyid.get();
+		}
+		else
+		{
+			//facultyid not found
+			throw new RuntimeException ("The Facultyid you've entered is invalid - " + theId);
+		}
+		
+		return theFaculty;
+	}
+
+	@Override
+	public void save(Faculty theFaculty) {
+		facultyRepository.save(theFaculty);
+
+	}
+
+	@Override
+	public void deleteById(int theId) {
+		facultyRepository.deleteById(theId);
+
+	}
+
+}
